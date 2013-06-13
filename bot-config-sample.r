887ba46158f8de958dc48ab1a8307c9d74e82b87
Adrian Sampaleanu <nmanole@gmail.com>
2013-06-13 16:36:57 -0400

    Added debug-room-id

--- bot-config-sample.r
+++ bot-config-sample.r
@@ -1,5 +1,6 @@
 botname: "@NameMe"
 room-id: 0
+debug-room-id: 0
 room-descriptor: "-- room name --"
 greet-message: "Welcome to the *** room.  See our [FAQ](https://host.com/faq)"
 low-rep-message: " Sorry, your reputation score is too low to chat at present.  Not our rules, but those of Stackoverflow.  If you were to [answer some questions](http://stackoverflow.com/questions/tagged/rebol), or [ask some](http://stackoverflow.com/questions/ask), we might be able to help by upvoting you.  In the meantime, have a look at a [Rebol introduction](http://rebol.com/rebolsteps.html)."
