5b83942bbd2acd4a7aa75eb25a274a1f5491d9b4
johnk- <john@johnandelizabeth.com>
2016-02-04 14:41:21 +1100

    Updated file to include authentication details

--- bot-config-sample.r
+++ bot-config-sample.r
@@ -1,8 +1,9 @@
+bot-user: "SO user to authenticate as - (must be a stackexchange SO user not a google sso)"
+bot-pass: "password for user"
+bot-room: http://chat.stackoverflow.com/rooms/<room-id>/<room-name>
 botname: "@NameMe"
 room-id: 0
-log-file: %log.txt
 room-descriptor: "-- room name --"
+log-file: %log.txt
 greet-message: "Welcome to the *** room.  See our [FAQ](https://host.com/faq)"
 low-rep-message: " Sorry, your reputation score is too low to chat at present.  Not our rules, but those of Stackoverflow.  If you were to [answer some questions](http://stackoverflow.com/questions/tagged/rebol), or [ask some](http://stackoverflow.com/questions/ask), we might be able to help by upvoting you.  In the meantime, have a look at a [Rebol introduction](http://rebol.com/rebolsteps.html)."
-bot-cookie: {}
-bot-fkey: ""
